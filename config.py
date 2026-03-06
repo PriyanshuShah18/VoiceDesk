8c0edbefe0fa10a7c3cfecfb43d9acb1f9cc53ef
PriyanshuShah18 <priyanshu18shah@gmail.com>
2026-03-06 17:10:41 +0530

    Config.py updated

--- config.py
+++ config.py
@@ -5,7 +5,12 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def get_secret(key,default=None):
-    if key in st.secrets:
-        return st.secrets[key]
+    try:
+        if key in st.secrets:
+            return st.secrets[key]
+    except Exception:
+        pass
+
     return os.getenv(key,default)
-    
+        
+        
